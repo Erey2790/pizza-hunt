443ab8d17f47e40d29f3a603c9a896244cfe976a
ElmerPC <erey2790@gmail.com>
2022-03-15 18:00:16 -0500

    added comment routes

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,3 +1,4 @@
+const { del } = require('express/lib/application');
 const { Comment, Pizza } = require('../models');
 
 const commentController = {
@@ -23,8 +24,26 @@ const commentController = {
     },
 
     // remove comment
-    removeComment() {
-
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+            .then(deleteComment => {
+                if (!deleteComment) {
+                    return res.status(404).json({ message: 'No comment with this id!' });
+                }
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $pull: { comments: params.commentId } },
+                    { new: true } 
+                );
+            })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
     }
 };
 
